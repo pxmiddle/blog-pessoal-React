222952d34d691c815e4a58ecfeff2108aa96163a
Pablo Henrique <pablo.h.s.maia@gmail.com>
2024-01-22 18:03:45 -0300

    Concluindo o CRUD

--- src/App.tsx
+++ src/App.tsx
@@ -6,6 +6,9 @@ import Navbar from "./components/navBar/NavBar";
 import Footer from "./components/footer/Footer";
 import Login from "./pages/login/Login";
 import { AuthProvider } from "./contexts/AuthContext";
+import FormularioTema from "./components/temas/formularioTema/FormularioTema";
+import ListaTemas from "./components/temas/listaTemas/ListaTemas";
+import DeletarTema from "./components/temas/deletarTema/DeletarTema";
 
 function App() {
   return (
@@ -19,6 +22,10 @@ function App() {
               <Route path="/home" element={<Home />} />
               <Route path="/cadastro" element={<Cadastro />} />
               <Route path="/login" element={<Login />} />
+              <Route path="/temas" element={<ListaTemas />} />
+              <Route path="/cadastroTema" element={<FormularioTema />} />
+              <Route path="/editarTema/:id" element={<FormularioTema />} />
+              <Route path="/deletarTema/:id" element={<DeletarTema />} />
             </Routes>
           </div>
           <Footer />
